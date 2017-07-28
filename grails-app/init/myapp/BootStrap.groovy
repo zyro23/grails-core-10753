1b4203039ce538c4fcc3ab94063a12df20949cb1
zyro <zyro@zyro.net>
2017-07-28 22:31:00 +0200

    sample app reproducing grails/grails-core#10753

--- grails-app/init/myapp/BootStrap.groovy
+++ grails-app/init/myapp/BootStrap.groovy
@@ -1,8 +1,17 @@
 package myapp
 
+import grails.core.GrailsApplication
+
+import javax.sql.DataSource
+
 class BootStrap {
 
+    DataSource dataSource
+    GrailsApplication grailsApplication
+
     def init = { servletContext ->
+        println grailsApplication.mainContext.getBeansOfType(DataSource)
+        println dataSource.dump()
     }
     def destroy = {
     }
